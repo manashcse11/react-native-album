ced316f8bc09ecb1396e89d5ade44b2bc330910a
Manash Chakrobortty <manash.pstu@gmail.com>
2017-12-23 13:28:01 +0600

    Add some style for Header

--- src/common/Header.js
+++ src/common/Header.js
@@ -10,6 +10,14 @@ const Header = () => (
 const styles = StyleSheet.create({
     viewStyle: {
         backgroundColor: '#F2F3F4',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
     },
     textStyle: {
         fontSize: 20
